4cd27aa77194e2109ff80950dbd409eed95438c2
LouisBertin <bertin.louis7@gmail.com>
2018-12-03 22:17:21 -0500

    init Java crawler

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import crawler.Crawler;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Crawler crawler = new Crawler();
+    }
+
+}
