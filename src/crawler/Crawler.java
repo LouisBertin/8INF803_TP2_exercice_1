6cb9ebfcd0f63a4b204af01d83c3aac6800b9b8f
LouisBertin <bertin.louis7@gmail.com>
2018-12-03 23:55:55 -0500

    convert formatted Json of monsters to Json file

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -1,20 +1,37 @@
 package crawler;
 
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Crawler {
+    // Current file directory
+    private final String CURRENT_DIR = System.getProperty("user.dir") + "/src/crawler/";
+
+    /**
+     * Constructor
+     */
     public Crawler() {
         ArrayList allLinks = getAllLinks();
-        getLinksData(allLinks);
+        JSONArray jsonArray = getLinksData(allLinks);
+        jsonToFile(jsonArray);
     }
 
+    /**
+     * return all monster links from legacy.aonprd.com
+     * @return ArrayList
+     */
     private ArrayList getAllLinks() {
         ArrayList links = new ArrayList();
         Document doc = null;
@@ -25,29 +42,81 @@ public class Crawler {
             e.printStackTrace();
         }
 
-        Elements newsHeadlines = doc.select("#monster-index-wrapper ul li a");
+        Elements newsHeadlines = doc.select("#monster-index-wrapper ul li");
         for (Element headline : newsHeadlines) {
-            String url = "http://legacy.aonprd.com/bestiary/" + headline.attr("href");
-            links.add(url);
+            String headlineChar = (String) headline.toString();
+            String test = Character.toString(headlineChar.charAt(4));
+            if (test.equals("<")) {
+                Element tag = Jsoup.parse(headlineChar, "", Parser.xmlParser());
+                String url = "http://legacy.aonprd.com/bestiary/" + tag.select("a").attr("href");
+                links.add(url);
+            }
         }
 
         return links;
     }
 
-    private void getLinksData(ArrayList links) {
+    /**
+     * return Json from each monster page
+     * @param links ArrayList
+     * @return JSONArray
+     */
+    private JSONArray getLinksData(ArrayList links) {
         // init json string
-        JSONObject jsonString = new JSONObject()
-                .put("JSON1", "Hello World!");
+        JSONArray jsonArray = new JSONArray();
 
-        for (int i = 0; i < 3; i++) {
+        // TODO : change 10 to links.size() when crawler is over
+        for (int i = 0; i < 10; i++) {
             try {
                 String url = (String) links.get(i);
                 Document doc = Jsoup.connect(url).get();
-                Elements monsterName = doc.select(".monster-header");
-                System.out.println(monsterName.text());
+                Elements monsterNames = doc.select(".stat-block-title > b");
+
+                for (Element element: monsterNames) {
+                    element.select(".stat-block-cr").remove();
+                    String monsterNameString = element.text();
+
+                    JSONObject monster = new JSONObject();
+                    monster.put("name", monsterNameString);
+                    monster.put("spells", "test");
+
+                    jsonArray.put(monster);
+                }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        System.out.println(jsonArray);
+        return jsonArray;
+    }
+
+    /**
+     * convert JsonArray to Json file
+     * @param jsonArray JSONArray
+     */
+    private void jsonToFile(JSONArray jsonArray) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, true);
+
+        String originalJson = jsonArray.toString();
+        JsonNode tree = null;
+        try {
+            tree = objectMapper .readTree(originalJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            String formattedJson = objectMapper.writeValueAsString(tree);
+
+            try (FileWriter file = new FileWriter(CURRENT_DIR + "monsters.json")) {
+                file.write(formattedJson);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
